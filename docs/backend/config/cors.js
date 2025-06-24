a303113430022537cb7908429c0439ea8f6d3e7c
Atxlla <46129217+Atxlla@users.noreply.github.com>
2025-06-24 18:33:41 +0200

    Initial commit

---
+++ docs/backend/config/cors.js
@@ -0,0 +1,14 @@
+const corsConfig = {
+    enabled: true,
+    origin: [
+      'http://localhost:8000',
+      'http://127.0.0.1:8000'
+    ],
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    headers: true,
+    exposeHeaders: ['Content-Disposition'],
+    credentials: true,
+  }
+  
+  export default corsConfig
+  
